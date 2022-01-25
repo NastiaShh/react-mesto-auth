37d37fc3d9e64a9d708119aa676a57d16f5fbc97
N.SH <sky.blush@ya.ru>
2022-01-25 21:46:48 +0300

    fix forms

--- src/components/App.js
+++ src/components/App.js
@@ -58,50 +58,44 @@ function App() {
         name='profile'
         isOpen={isEditProfilePopupOpen}
         onClose={closeAllPopups}
-        children={
-          <>
-            <input id="name-input" type="text" name="name" placeholder="Жак-Ив Кусто" required className="popup__input popup__input_profile_name" minLength="2" maxLength="40" />
-            <span className="popup__input-error name-input-error"></span>
-            <input id="description-input" type="text" name="description" placeholder="Исследователь океана" required className="popup__input popup__input_profile_description" minLength="2" maxLength="200" />
-            <span className="popup__input-error description-input-error"></span>
-            <button type="submit" className="popup__save popup__save_profile">Сохранить</button>
-          </>
-        }
-      />
+        buttonText='Сохранить'>
+
+        <input id="name-input" type="text" name="name" placeholder="Жак-Ив Кусто" required className="popup__input popup__input_profile_name" minLength="2" maxLength="40" />
+        <span className="popup__input-error name-input-error"></span>
+        <input id="description-input" type="text" name="description" placeholder="Исследователь океана" required className="popup__input popup__input_profile_description" minLength="2" maxLength="200" />
+        <span className="popup__input-error description-input-error"></span>
+        {/* <button type="submit" className="popup__save popup__save_profile">Сохранить</button> */}
+      </PopupWithForm>
+
       <PopupWithForm
         title='Новое место'
         name='place'
         isOpen={isAddPlacePopupOpen}
         onClose={closeAllPopups}
-        children={
-          <>
-            <input id="place-input" type="text" name="name" placeholder="Название" required className="popup__input popup__input_place_name" minLength="2" maxLength="30" />
-            <span className="popup__input-error place-input-error"></span>
-            <input id="link-input" type="url" name="description" placeholder="Ссылка на картинку" required className="popup__input popup__input_place_description" />
-            <span className="popup__input-error link-input-error"></span>
-            <button type="submit" className="popup__save popup__save_place">Создать</button>
-          </>
-        }
-      />
+        buttonText='Создать'>
+
+        <input id="place-input" type="text" name="name" placeholder="Название" required className="popup__input popup__input_place_name" minLength="2" maxLength="30" />
+        <span className="popup__input-error place-input-error"></span>
+        <input id="link-input" type="url" name="description" placeholder="Ссылка на картинку" required className="popup__input popup__input_place_description" />
+        <span className="popup__input-error link-input-error"></span>
+      </PopupWithForm>
+
       <PopupWithForm
         title='Обновить аватар'
         name='change-avatar'
         isOpen={isEditAvatarPopupOpen}
         onClose={closeAllPopups}
-        children={
-          <>
-            <input id="avatar-input" type="url" name="avatar" placeholder="Ссылка на картинку" required className="popup__input popup__input_change-avatar" />
-            <span className="popup__input-error avatar-input-error"></span>
-            <button type="submit" className="popup__save popup__save_change-avatar">Сохранить</button>
-          </>
-        }
-      />
+        buttonText='Сохранить'>
+
+        <input id="avatar-input" type="url" name="avatar" placeholder="Ссылка на картинку" required className="popup__input popup__input_change-avatar" />
+        <span className="popup__input-error avatar-input-error"></span>
+      </PopupWithForm>
 
       <ImagePopup
         card={selectedCard}
         onClose={closeAllPopups}
       />
-  
+
     </>
   )
 }
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,18 +1,15 @@
 function PopupWithForm(props) {
   return (
-    <>
-      <div className={`popup popup_${props.name} ${props.isOpen ? 'popup_open' : ''}`}>
-        <div className={`popup__content popup__content_form popup__content_${props.name}`}>
-         <button type="button" className="popup__close" aria-label="Закрыть окно" onClick={props.onClose}></button>
-         <h3 className={`popup__title popup__title_${props.name}`}>{props.title}</h3>
-          <form name={`form_${props.name}`} className="popup__form" noValidate>
-
-            {props.children}
-
-          </form>
-        </div>
+    <div className={`popup popup_${props.name} ${props.isOpen ? 'popup_open' : ''}`}>
+      <div className={`popup__content popup__content_form popup__content_${props.name}`}>
+        <button type="button" className="popup__close" aria-label="Закрыть окно" onClick={props.onClose}></button>
+        <h3 className={`popup__title popup__title_${props.name}`}>{props.title}</h3>
+        <form name={`form_${props.name}`} className="popup__form">
+          {props.children}
+          <button type="submit" className="popup__save popup__save_${props.name}">{props.buttonText}</button>
+        </form>
       </div>
-    </>
+    </div>
   )
 }
 
